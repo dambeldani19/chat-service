1279e038f8c09d8cbe931252d793f002ae83fc49
dambeldani <dambeldani@gmail.com>
2024-08-22 09:30:20 +0700

    remove auth middleware custome api test

--- test/api_test.go
+++ test/api_test.go
@@ -19,31 +19,6 @@ import (
 	"gorm.io/gorm"
 )
 
-// AuthMiddleware extracts the userID from the Authorization header
-func AuthMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		authHeader := c.GetHeader("Authorization")
-
-		if authHeader == "" {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is required"})
-			c.Abort()
-			return
-		}
-
-		parts := strings.Split(authHeader, " ")
-
-		if len(parts) != 2 || parts[0] != "Bearer" {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid Authorization header format"})
-			c.Abort()
-			return
-		}
-
-		userID := parts[1]
-		c.Set("userID", userID)
-		c.Next()
-	}
-}
-
 // SetupRouter for testing
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
